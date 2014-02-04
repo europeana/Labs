cd1e5856517e68cdc397bb7d327504cab174e0ac
alen <alen.bratanovic@gmail.com>
2014-02-04 16:11:12 +0100

    - back to search results

--- js/labs.js
+++ js/labs.js
@@ -8,6 +8,9 @@ $(document).ready(function(){
 	
 	$("#homepage ul.nav-footer").append("<br class='clear'/>");
 	
+	if(document.referrer.indexOf("/search") != -1 && document.URL.indexOf("/search")==-1){
+		$("#search").append("<a style='display:block; font-size:0.8em; text-align:center;' href='"+document.referrer+"'>Back to search results</a>");
+	}
 });
 
 var semLabs = {
