9253dfed1305e12b1dc38a577b8266a977ffe81c
alenbratanovic <alen.bratanovic@gmail.com>
2014-04-16 17:14:12 +0200

    expired events filter

--- _plugins/sortEventByDate.rb
+++ _plugins/sortEventByDate.rb
@@ -1,7 +1,8 @@
 module Jekyll
   module Filters
     def sortEventByDate(events)
-      events.sort_by {|x| Date.strptime(x.data['startdate'].first,"%d/%m/%Y")}
+      sortirani = events.sort_by {|x| Date.strptime(x.data['startdate'].first,"%d/%m/%Y")}
+      sortirani.select{|x| Date.strptime(x.data['startdate'].first,"%d/%m/%Y") > DateTime.now.to_date}
     end        
   end
 end
