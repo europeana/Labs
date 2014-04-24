1921aa103e542031a5902bacb15101b3bf015353
alenbratanovic <alen.bratanovic@gmail.com>
2014-04-24 10:09:45 +0200

    events archive

---
+++ _plugins/sortEventByDateArchive.rb
@@ -0,0 +1,8 @@
+module Jekyll
+  module Filters
+    def sortEventByDateArchive(events)
+      sortirani = events.sort_by {|x| Date.strptime(x.data['startdate'].first,"%d/%m/%Y")}
+      sortirani.select{|x| Date.strptime(x.data['startdate'].first,"%d/%m/%Y") <= DateTime.now.to_date}
+    end        
+  end
+end
