75ab6363ee873d0b8a304b8b4d7aa5cbe122abba
Semantika Build Server <info@semantika.si>
2015-01-09 13:03:00 +0100

    Update

--- _plugins/sortEventByDateArchive.rb
+++ _plugins/sortEventByDateArchive.rb
@@ -2,7 +2,7 @@ module Jekyll
   module Filters
     def sortEventByDateArchive(events)
       sortirani = events.sort_by {|x| Date.strptime(x.data['startdate'].first,"%d/%m/%Y")}
-      sortirani.select{|x| Date.strptime(x.data['enddate'].first,"%d/%m/%Y") <= DateTime.now.to_date}
+      sortirani.select{|x| Date.strptime(x.data['enddate'].nil? ? x.data['startdate'].first : x.data['enddate'].first,"%d/%m/%Y") <= DateTime.now.to_date}
     end        
   end
 end
